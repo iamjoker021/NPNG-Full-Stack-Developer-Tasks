5ea1b273e7451917778be2fe52d728f51e22ff66
iamjoker021 <iamjoker021@gmail.com>
2024-09-04 11:15:01 +0530

    doc: Update Sep4, task1 to create btn via JS itself

--- change-bg-js/script.js
+++ change-bg-js/script.js
@@ -1,4 +1,16 @@
+// Create button
+const createBtn = () => {
+    const btn = document.createElement('button');
+    btn.type = 'button';
+    btn.textContent = 'Swicth Color to black';
+    return btn;
+}
+
+const btn = createBtn();
+document.querySelector('body').appendChild(btn);
+
 document.querySelector('button')
 .addEventListener('click', (e) => {
     document.querySelector('body').style.backgroundColor = (document.querySelector('body').style.backgroundColor === 'black') ? 'white' : 'black';
+    e.target.textContent = e.target.textContent === 'Swicth Color to black' ? 'Swicth Color to white' : 'Swicth Color to black';
 })
